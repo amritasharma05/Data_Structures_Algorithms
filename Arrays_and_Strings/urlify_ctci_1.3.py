fa757f813119ab6ff8851cc6eac77ddde5b6c2cc
amritasharma05 <amrita9d@gmail.com>
2020-05-07 12:11:14 -0700

    renamed files and added dsa code

---
+++ Arrays_and_Strings/urlify_ctci_1.3.py
@@ -0,0 +1,23 @@
+"""
+Write a method to replace all spaces in string with "%20"
+input = "Mr John Smith    ", 12
+output = "Mr%20John%20Smith
+
+Approach - Use
+Time Complexity - O(N)
+Space Complexity - O(N)
+"""
+
+
+def urlify(st,st_len):
+    # Trim the string to remove trailing whitespaces
+    st = st.strip()
+    st = st.replace(" ","%20")
+    return st
+
+
+# Checking trailing spaces
+print(urlify("Mr John Smith    ",12))
+
+# Checking white spaces at start
+print(urlify("      My name is John Doe", 18))
